21b4536054e6e9b7ce4d83fde53889959fced1be
Lyle Foster <foster.lyle@gmail.com>
2017-07-10 12:14:38 +0400

    added exercise solutions for chapter9: more stuff

---
+++ chapter_9_more_stuff/exercise_3.rb
@@ -0,0 +1,4 @@
+# What is exception handling and what problem does it solve?
+
+# exception handling is the use of begin, rescue and end to catch possible errors to allow the program to continue working rather than to stop due to encoutering something it was not able to run
+
