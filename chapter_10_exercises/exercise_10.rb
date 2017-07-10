68beec2b18376d533b33bd53992c1ac29d89554a
Lyle Foster <foster.lyle@gmail.com>
2017-07-10 14:01:11 +0400

    added exercise solution for chapter 10: Exercises

---
+++ chapter_10_exercises/exercise_10.rb
@@ -0,0 +1,9 @@
+# Can hash values be arrays? Can you have an array of hashes? (give examples)
+
+# yes and yes
+
+hash = {
+  pet: ['nala', 'simba']
+}
+
+array = [{pet: 'nala'}, {pet: 'simba'}]
---
+++ chapter_10_exercises/exercise_7.rb
@@ -0,0 +1,4 @@
+# What's the major difference between an Array and a Hash?
+
+# Array's are ordered collections of objects, whereas a Hash contains key value pairs that can be referenced by their key
+
